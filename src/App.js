c4ca45625e2398f5bbd22a5af343a12889662f7e
Maneeshaprabha <maneeprabha28@gmail.com>
2024-01-16 20:04:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+// import logo from './logo.svg';
+import './App.css';
+import Pages from './components/pages/Pages';
+
+function App() {
+  return  <><Pages/></>
+}
+
+export default App;
---
+++ src/components/pages/Pages.jsx
@@ -0,0 +1,33 @@
+import React from "react"
+import Header from "../common/header/Header"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Home from "../home/Home"
+import Footer from "../common/footer/Footer"
+import About from "../about/About"
+import Services from "../services/Services"
+import Blog from "../blog/Blog"
+import Princing from "../princing/Princing"
+import Contact from "../contact/Contact"
+
+
+const Pages = () => {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/services' component={Services} />
+        <Route exact path='/blog' component={Blog} />
+        <Route exact path='/pricing' component={Princing} />
+        <Route exact path='/contact' component={Contact} />
+        
+        </Switch>
+        <Footer/>
+      </Router>
+    </>
+  )
+}
+
+export default Pages
